7283d2f4531b5ec62d9e0096f2d55ec14590fb4c
fajarsiagian <fajar7xx@gmail.com>
2020-10-07 07:29:51 +0700

    tambah card product dan penyelesaian tampilan home

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <footer class="mt-5 mb-5">
+      <hr>
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            2030 Copyright FajarSiagian
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,21 @@
     <Navbar />
     <div class="container">
       <Hero />
+      <div class="row mt-4">
+        <div class="col">
+          <h2>Best <strong>Foods</strong></h2>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-success float-right">
+            <b-icon-eye></b-icon-eye> Lihat Semua
+          </router-link>
+        </div>
+      </div>
+      <div class="row mb-4">
+        <div class="col-md-4 mt-4" v-for="product in products" :key="product.id">
+          <CardProduct :product="product"/>
+        </div>
+      </div>
     </div>
     <!-- <h1>Home</h1> -->
   </div>
@@ -13,15 +28,42 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-import Navbar from '@/components/Navbar.vue'
-import Hero from '@/components/Hero.vue'
+import axios from "axios";
+
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import CardProduct from "@/components/CardProduct.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     // HelloWorld
     Navbar,
-    Hero
-  }
-}
+    Hero,
+    CardProduct,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    setProduct(data) {
+      this.products = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/best-products")
+      .then((response) =>
+        // handle success
+        // console.log("berhasil : ", response)
+        this.setProduct(response.data)
+      )
+      .catch((error) =>
+        // handle error
+        console.log("gagal : ", error)
+      )
+  },
+};
 </script>
